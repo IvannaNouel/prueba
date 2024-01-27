a5afd2e55ef88057f6dc0214a46932a60fe31778
IvannaNouel <ivannamarcelle@gmail.com>
2024-01-27 10:56:32 -0600

    cada 24 hr
    -Falta agregar que cargue a la base de datos

--- 03- Prueba/pruebaETL/pruebaETL/__init__.py
+++ 03- Prueba/pruebaETL/pruebaETL/__init__.py
@@ -1,1 +1,23 @@
-from .assets import excel_data, mysql_table_created, data_loaded_in_mysql
+from dagster import (
+    AssetSelection,
+    Definitions,
+    ScheduleDefinition,
+    define_asset_job,
+    load_assets_from_modules,
+)
+
+from . import assets
+
+all_assets = load_assets_from_modules([assets])
+
+jobs = define_asset_job("jobs", selection=AssetSelection.all())
+
+jobs_schedule = ScheduleDefinition(
+    job=jobs,
+    cron_schedule="0 0 * * *",  
+)
+
+defs = Definitions(
+    assets=all_assets,
+    schedules=[jobs_schedule],
+)
